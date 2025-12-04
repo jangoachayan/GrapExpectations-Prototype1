703910e4e90e6f7c3844702042cf5f0a242a7fd0
Ashwin Samuel <ashwin.ashwinsamuel@gmail.com>
2025-12-04 20:47:54 +0530

    feat: Set up initial Android project with Kotlin, Jetpack Compose, Firebase, and MPAndroidChart.

---
+++ android/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GrapExpectations-Prototype1"
+include(":app")
